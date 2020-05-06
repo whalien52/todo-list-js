ce11d3b8a3c06e0ceb060f36b7c5a4c087b723f4
Kinsey <kinseybasinger@gmail.com>
2020-05-06 16:50:49 -0500

    Add files via upload

--- appToDo.js
+++ appToDo.js
@@ -1,15 +1,24 @@
 document.addEventListener('DOMContentLoaded', function() {
+    loadFromLocalStorage();
     let list = document.querySelector('ul');
-    console.log(list);
     let btn = document.querySelector('button');
     btn.addEventListener('click', function(e) {
-        e.preventDefault();
-        let todo = document.querySelector('input[name="todoitem"]');
-        let listItem = document.createElement('li');
-        listItem.innerText = todo.value;
-        list.append(listItem);
-    })
-    
+        let todo = document.querySelector('input[name="todoitem"]').value;
+        addItem(list, todo);
+        saveToDo(todo, '');
+        todo.value = '';
+    });
+
+    function addItem(list,todo, status) {
+      let listItem = document.createElement('li');
+      listItem.innerText = todo;
+      if (status) {
+        listItem.classList = status;
+      }
+      list.append(listItem);
+    }
+
+
     list.addEventListener('click', function(e){
        if (e.target.tagName == 'LI') {
            if (e.target.className == 'done') {
@@ -18,11 +27,50 @@ document.addEventListener('DOMContentLoaded', function() {
                    e.target.className = 'done';
                }
            }
+           var itemText = e.target.innerText;
+           saveToDo(itemText, 'done');
     });
-    
+
     list.addEventListener('dblclick', function(e){
        if (e.target.tagName == 'LI') {
+          var todoItem = e.target.innerText;
            e.target.remove();
+           localStorage.removeItem(todoItem);
            }
     });
+
+    function saveToDo(todoValue, status) {
+        var item = {todoName: todoValue, todoStatus: status};
+
+        if (localStorage.getItem(todoValue)) {
+            var stored = JSON.parse(localStorage.getItem(todoValue));
+            if (status != stored["todoStatus"]) {
+              localStorage.setItem(todoValue, JSON.stringify(item));
+              console.log("updated");
+            }
+        } else {
+            localStorage.setItem(todoValue, JSON.stringify(item));
+            console.log('added!');
+            }
+
+    }
+
+    function adjustStatus(e, status) {
+      e.target.className = status;
+    }
+
+    function loadFromLocalStorage() {
+      let list = document.querySelector('ul');
+        if (localStorage.length > 0) {
+          for (item in localStorage) {
+            var todoItem = JSON.parse(localStorage.getItem(item));
+            if (typeof todoItem === 'object' && todoItem !== null) {
+              addItem(list, todoItem['todoName'], todoItem['todoStatus']);
+            }
+          }
+        } else {
+          console.log('storage empty');
+        }
+    }
+
 });
